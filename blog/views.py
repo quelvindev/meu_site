ad08c1d4043417cf3cfabaf70ad85e63a61c28f5
quelvindev <quelvindev@gmail.com>
2024-06-18 23:57:57 -0300

    consumindo jsonplacerolader

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.data import posts
 
 # Create your views here.
 
+context ={
+    'text':'Dados serão atualizados',
+    'posts':posts
+}
+
 def blog(request):
     print('chegou na view')
-    return render(request,'blog/blog.html')
+    return render(request,'blog/blog.html',context)
 
 
 def exemplo(request):
